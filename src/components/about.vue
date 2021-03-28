c0493b4763d3b4d10d643f570c23c31754c62a94
turulomio <turulomio@yahoo.es>
2021-03-28 08:48:48 +0200

    Works with i18n parameters

--- src/components/about.vue
+++ src/components/about.vue
@@ -4,6 +4,7 @@
         <p class="text-justify">This project is in <a href="https://github.com/turulomio/vipcontacts">Github</a></p>
         <p>{{ $t('message') }}</p>
         <p>{{ $t('hola') }}</p>
+        <p>{{ $t('hola inside') }}</p>
         <p>{{ $t('This is a text') }}</p>
         <p>{{ $t('company', {company: "Google"}) }}</p>
     </div>
@@ -17,7 +18,10 @@
 <i18n>
 {
   "en": {
-    "hola": "Hello i18n in SFC!"
+    "hola inside": "Hello i18n in SFC!"
+  },
+  "es": {
+    "hola inside": "Hola i18n desde dentro SFC!"
   }
 }
 </i18n>
