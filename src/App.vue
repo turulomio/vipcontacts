a7c030142cbdd047aa493957f494e17949eec408
turulomio <turulomio@yahoo.es>
2021-03-31 18:01:49 +0200

    Changed icon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <v-app-bar app color="primary" dark fixed  fill-height >   
-            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
+            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/vip.svg" transition="scale-transition" width="50" height="50" />
             <h1>Vip Contacts</h1>
             <v-spacer />
             <Menu/>
