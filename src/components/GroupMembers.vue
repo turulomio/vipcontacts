2d7fb11eca44bbb28064870f1425afe5105c1fec
turulomio <turulomio@yahoo.es>
2021-04-17 14:24:14 +0200

    Allow to include or exclude group members

--- src/components/GroupMembers.vue
+++ src/components/GroupMembers.vue
@@ -2,6 +2,8 @@
     <div>
         <h1>{{ $t("GroupMembers") }}</h1>
         <AutoCompleteApiOneField v-model="group" :label="$t('Select a group')" :placeholder="$t('Enter a string to search a group')" :apiurl="`${this.$store.state.apiroot}/api/groups/`" field="name" @input="refresh_members" />
+        
+        <v-switch v-model="members" :label="$t('Show group members')" @change="refresh_members"></v-switch>
         <v-data-table :headers="headers" :items="data" sort-by="name" class="elevation-1" enabled="i">
             <template v-slot:top>
                 <v-toolbar flat>
@@ -11,8 +13,8 @@
                 </v-toolbar>
             </template>
             <template v-slot:[`item.actions`]="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-                <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+                <v-icon small class="mr-2" @click="add_to_group(item)" v-show="!members">mdi-plus</v-icon>
+                <v-icon small @click="remove_from_group(item)" v-show="members">mdi-minus</v-icon>
             </template>
         </v-data-table>
     </div>
@@ -39,12 +41,13 @@
                     { text: this.$t('Birth date'), value: 'birth' },
                     { text: this.$t('Actions'), value: 'actions', sortable: false },
                 ],
+                members: true,
             }
         },          
         methods: {
             refresh_members: function() {
                 console.log("refresh_members")
-                axios.get(`${this.$store.state.apiroot}/api/groups/members/?search=${this.group}`, { headers: {'Authorization': `Token ${this.$store.state.token}`   }})
+                axios.get(`${this.$store.state.apiroot}/api/groups/members/?search=${this.group}&members=${this.members}`, { headers: {'Authorization': `Token ${this.$store.state.token}`   }})
                 .then((response) => {
                     this.data= response.data;
                     //this.PersonEdit_refreshKey();
@@ -52,6 +55,9 @@
                 }, (error) => {
                     console.log(error);
                 });
+            },
+            editItem (item) {
+                this.$router.replace({ name: 'person_edit', params: { "id": item.id }})
             },
         }
     }
