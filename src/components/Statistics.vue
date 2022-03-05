91cc30e9a62df28c235de35c47f46ea46cdcba0a
turulomio <turulomio@yahoo.es>
2022-03-05 07:35:39 +0100

    Improved statistics

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -2,9 +2,43 @@
     <div v-show="this.$store.state.logged">
         <div class="login">
             <h1>{{ $t("Statistics") }}</h1>
-            <v-card>
-            <chart-pie :name="$t('Vip Contacts registers')" :items="items" height="600px" :key="key"></chart-pie>
-            </v-card>
+
+            <v-tabs  background-color="primary" dark v-model="tab" next-icon="mdi-arrow-right-bold-box-outline" prev-icon="mdi-arrow-left-bold-box-outline" show-arrows>
+                <v-tab key="Gender"><v-icon small style="margin:6px">mdi-gender-male</v-icon>{{ $t("Gender")}}</v-tab>
+                <v-tab key="Jobs"><v-icon small style="margin:6px">mdi-rename-box</v-icon>{{ $t("Jobs")}}</v-tab>
+                <v-tab key="Countries"><v-icon small style="margin:6px">mdi-rename-box</v-icon>{{ $t("Countries")}}</v-tab>
+                <v-tab key="Cities"><v-icon small style="margin:6px">mdi-rename-box</v-icon>{{ $t("Cities")}}</v-tab>
+                <v-tab key="Registers"><v-icon small style="margin:6px">mdi-rename-box</v-icon>{{ $t("Registers")}}</v-tab>
+
+                <v-tabs-slider color="yellow"></v-tabs-slider>
+            </v-tabs>
+            <v-tabs-items v-model="tab" v-if="statistics">
+                <v-tab-item key="Gender">
+                    <v-card flat>
+                      <chart-pie :name="$t('Gender')" :items="statistics.gender" height="600px" :key="key"></chart-pie>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item key="Jobs">
+                    <v-card flat>
+                      <chart-pie :name="$t('Jobs')" :items="statistics.jobs" height="600px" :key="key"></chart-pie>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item key="Countries">
+                    <v-card flat>
+                      <chart-pie :name="$t('Countries')" :items="statistics.countries" height="600px" :key="key"></chart-pie>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item key="Cities">
+                    <v-card flat>
+                      <chart-pie :name="$t('Cities')" :items="statistics.cities" height="600px" :key="key"></chart-pie>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item key="Registers">
+                    <v-card flat>
+                      <chart-pie :name="$t('Vip Contacts registers')" :items="statistics.registers" height="600px" :key="key"></chart-pie>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
         </div>
     </div>
 </template>
@@ -19,8 +53,9 @@
         },
         data () {
             return {
-                items:[],
+                statistics:null,
                 key:0,
+                tab:0,
             }
         },
         methods: {
@@ -28,7 +63,8 @@
                 axios.get(`${this.$store.state.apiroot}/api/statistics/`, this.myheaders())
                 .then((response) => {
                     this.parseResponse(response)
-                    this.items=response.data 
+                    this.statistics=response.data 
+                    console.log(this.statistics)
                 }, (error) => {
                     this.parseResponseError(error)
                 })
