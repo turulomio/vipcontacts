ed3da07624b609de4f68b8b7d853883c2be4137d
turulomio <turulomio@yahoo.es>
2022-03-28 16:13:43 +0200

    Compiles with eslint

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,6 @@ module.exports = {
         enableInSFC: true
         }
     },  
-    devServer: {
-        open: process.platform === 'darwin',
-        host: '0.0.0.0',
-        port: 8080, 
-        hotOnly: false,    
-    },
-
     publicPath: process.env.VUE_APP_PUBLIC_PATH
 
 }
