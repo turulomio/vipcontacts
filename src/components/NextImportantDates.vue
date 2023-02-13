bb8da472ff4631b3d89eaa34562e662c52e4c8d1
turulomio <turulomio@yahoo.es>
2023-02-13 14:38:45 +0100

    Sorting by current date

--- src/components/NextImportantDates.vue
+++ src/components/NextImportantDates.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="pa-5">
         <h1>{{ $t('Next important dates') }}</h1>
-        <v-data-table :headers="headers" :items="dates" :sort-by="['date']" :sort-desc="['date']" class="elevation-1" @click:row="viewItem">
+        <v-data-table :headers="headers" :items="dates" :sort-by="['date2']" :sort-desc="[true]" class="elevation-1" @click:row="viewItem">
+            <template v-slot:[`item.date2`]="{ item }">{{ changeYear(item) }}</template>    
         </v-data-table>
     </div>
 </template>
@@ -16,7 +17,7 @@
             return{
                 loading:false,
                 headers: [
-                    { text: this.$t('Date'), align: 'start', sortable: true, value: 'date', width:"10%"},
+                    { text: this.$t('Date'), align: 'start', sortable: true, value: 'date2', width:"10%"},
                     { text: this.$t('Name'), value: 'person', width:"45%" },
                     { text: this.$t('Reason'), value: 'reason', width:"45%"},
                 ],
@@ -26,6 +27,18 @@
             viewItem (item) {
                 this.$router.replace({ name: 'person_edit', params: { "id": item.id }})
             },
+            changeYear(item){
+                console.log("chasnge")
+                var o=new Date(item.date)
+                console.log(o)
+                var today=new Date()
+                console.log(today)
+                if (o.getMonth()>=today.getMonth()){
+                    return new Date(today.getFullYear(),o.getMonth(),o.getDate()+1).toISOString().slice(0,10)
+                } else {
+                    return new Date(today.getFullYear()+1,o.getMonth(),o.getDate()+41).toISOString().slice(0,10)
+                }
+            }
         }
     }
 </script>
